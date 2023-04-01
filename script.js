1f97a82387ce03a035ab185a34cc4910f351185c
Jean Barreto <jean@pwx.cloud>
2023-04-02 00:16:14 +0200

    add prettier, remove duplicated code, performance

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-const iris = document.getElementsByClassName('ball');
+const ball = document.getElementsByClassName('ball');
 document.onmousemove = (event) => {
   const x = (event.clientX * 100) / window.innerWidth + '%';
   const y = (event.clientY * 100) / window.innerHeight + '%';
 
-  for (let i = 0; i < 2; i++) {
-    iris[i].style.left = x;
-    iris[i].style.top = y;
-    iris[i].transform = 'translate(-' + x + ',-' + y + ')';
+  if (ball.length > 0) {
+    for (let i = 0; i < 2; i++) {
+      ball[i].style.left = x;
+      ball[i].style.top = y;
+      ball[i].transform = 'translate(-' + x + ',-' + y + ')';
+    }
   }
 };
-
