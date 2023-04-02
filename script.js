037a94b4907f430e051cd0a10d7bee3b0e5a2b29
Jean Barreto <jean@pwx.cloud>
2023-04-02 02:33:37 +0200

    add cards height function and project descriptions

--- script.js
+++ script.js
@@ -4,10 +4,37 @@ document.onmousemove = (event) => {
   const y = (event.clientY * 100) / window.innerHeight + '%';
 
   if (ball.length > 0) {
-    for (let i = 0; i < 2; i++) {
+    for (let i = 0; i < ball.length; i++) {
       ball[i].style.left = x;
       ball[i].style.top = y;
       ball[i].transform = 'translate(-' + x + ',-' + y + ')';
     }
   }
 };
+
+function setEqualHeight() {
+  const cards = document.querySelectorAll('.project-card');
+  let maxHeight = 0;
+
+  // Reset card body heights to auto
+  cards.forEach((card) => {
+    card.querySelector('.card-body').style.height = 'auto';
+  });
+
+  // Find the tallest card body
+  cards.forEach((card) => {
+    const cardBodyHeight = card.querySelector('.card-body').offsetHeight;
+    if (cardBodyHeight > maxHeight) {
+      maxHeight = cardBodyHeight;
+    }
+  });
+
+  // Set all card bodies to the tallest height
+  cards.forEach((card) => {
+    card.querySelector('.card-body').style.height = `${maxHeight}px`;
+  });
+}
+
+// Call the function on window load and resize events
+window.addEventListener('load', setEqualHeight);
+window.addEventListener('resize', setEqualHeight);
